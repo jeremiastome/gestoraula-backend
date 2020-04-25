c24a6448c3bd1d14e1eb639f425a44da56628ff0
Esteban Matas <estebanmatas13@gmail.com>
2020-04-25 03:42:50 -0300

    Agregado endpoint para modificar curso

--- src/main/java/com/tip/alumnos/controller/CursoController.java
+++ src/main/java/com/tip/alumnos/controller/CursoController.java
@@ -1,11 +1,13 @@
 package com.tip.alumnos.controller;
 
+import com.tip.alumnos.model.Asistencia;
 import com.tip.alumnos.model.Curso;
 import com.tip.alumnos.repository.IAlumnoRepository;
 import com.tip.alumnos.repository.ICursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -25,4 +27,10 @@ public class CursoController {
         cursoRepository.save(curso);
     }
 
+    @PutMapping("/cursos/{id}")
+    public void modificarCurso(@RequestBody List<Asistencia> asistencias, @PathVariable int id) {
+        Curso cursoAModificar = cursoRepository.findById(id).get();
+        cursoAModificar.getAsistencias().addAll(asistencias);
+        cursoRepository.save(cursoAModificar);
+    }
 }
